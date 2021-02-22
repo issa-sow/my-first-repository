92b4c060425ccdac9c30bc2a8ec51aa2dae79be3
Sow Issa <issasow25@gmail.com>
2021-02-22 23:34:16 +0000

    ajout de la mehode calculMoy()

--- src/Etudiant.java
+++ src/Etudiant.java
@@ -3,11 +3,17 @@ public class Etudiant {
 	private String nom;
 	private String prenom;
 	private String niveau;
+	private double note1;
+	private double note2;
+	private double moy;
 	
-	public Etudiant(String nom, String prenom, String niveau) {
+	public Etudiant(String nom, String prenom, String niveau, double note1, double note2) {
 		this.nom = nom;
 		this.prenom =prenom;
 		this.niveau = niveau;
+		this.note1 = note1;
+		this.note2 = note2;
+		this.moy = calculMoy();
 	}
 	
 	public String getNom() {
@@ -22,10 +28,25 @@ public class Etudiant {
 		return this.niveau;
 	}
 	
+	public double getNote1() {
+		return this.note1;
+	}
+	
+	public double getNote2() {
+		return this.note2;
+	}
+	
+	public double calculMoy() {
+		return (this.note1 + this.note2) / 2;
+	}
+	
 	public String toString() {
 		return "Nom : " + this.nom + "\n" + 
 				"Prenom : " + this.prenom + "\n" +
-				"Niveau : " + this.niveau ;
+				"Niveau : " + this.niveau + "\n" +
+				"Note 1 : " + this.note1 + "\n" + 
+				"Note 2 : " + this.note2 + "\n" + 
+				"Moyenne : " + this.moy;
 	}
 
 }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Etudiant etu = new Etudiant("Sall", "Moustapha", "Master 1"); 
+		Etudiant etu = new Etudiant("Sall", "Moustapha", "Master 1", 14, 13); 
 		System.out.println(etu);
 	}
 
