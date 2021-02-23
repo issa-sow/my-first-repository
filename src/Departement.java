586550c6b15f09858c5a3dd90a631992975746e9
Sow Issa <issasow25@gmail.com>
2021-02-23 16:35:15 +0000

    Modification des infromation de l'etudiant: students[0]

--- src/Departement.java
+++ src/Departement.java
@@ -10,6 +10,30 @@ public class Departement {
 		this.adresse = adresse;
 	}
 	
+	public String getNomDep() {
+		return this.nomDep;
+	}
+	
+	public void setNomDep(String nomDep) {
+		this.nomDep = nomDep;
+	}
+	
+	public String getTel() {
+		return this.tel;
+	}
+	
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	
+	public String getAdresse() {
+		return this.adresse;
+	}
+	
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	
 	public String toString() {
 		return "\t" + "Departement { " + "\n" +
 				"\t\t" + "Nom departement : " + this.nomDep + "\n" + 
